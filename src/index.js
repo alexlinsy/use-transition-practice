c630a193e003764e763a6e8cbdbf391e0e3f2cf3
alexlinsy <39345015+alexlinsy@users.noreply.github.com>
2020-05-25 19:09:07 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,100 @@
+import React, {
+  Suspense,
+  useState,
+  useTransition
+} from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+import { fetchProfileData } from "./fakeApi";
+
+const initialResource = fetchProfileData();
+
+function Button({ children, onClick }) {
+  const [
+    startTransition,
+    isPending
+  ] = useTransition({
+    timeoutMs: 10000
+  });
+
+  function handleClick() {
+    startTransition(() => {
+      onClick();
+    });
+  }
+
+  const spinner = (
+    <span
+      className="fa fa-circle-o-notch fa-spin"
+      style={{
+        marginLeft: 4,
+        fontSize: "small",
+        visibility: isPending
+          ? "visible"
+          : "hidden"
+      }}
+    />
+  );
+
+  return (
+    <>
+      <button
+        onClick={handleClick}
+        disabled={isPending}
+      >
+        {children}
+      </button>
+      {isPending ? spinner : null}
+    </>
+  );
+}
+
+function ProfilePage() {
+  const [resource, setResource] = useState(
+    initialResource
+  );
+
+  function handleRefreshClick() {
+    setResource(fetchProfileData());
+  }
+
+  return (
+    <Suspense
+      fallback={<h1>Loading profile...</h1>}
+    >
+      <ProfileDetails resource={resource} />
+      <Button onClick={handleRefreshClick}>
+        Refresh
+      </Button>
+      <Suspense
+        fallback={<h1>Loading posts...</h1>}
+      >
+        <ProfileTimeline resource={resource} />
+      </Suspense>
+    </Suspense>
+  );
+}
+
+function ProfileDetails({ resource }) {
+  const user = resource.user.read();
+  return <h1>{user.name}</h1>;
+}
+
+function ProfileTimeline({ resource }) {
+  const posts = resource.posts.read();
+  return (
+    <ul>
+      {posts.map(post => (
+        <li key={post.id}>{post.text}</li>
+      ))}
+    </ul>
+  );
+}
+
+const rootElement = document.getElementById(
+  "root"
+);
+ReactDOM.createRoot(rootElement).render(
+  <ProfilePage />
+);
